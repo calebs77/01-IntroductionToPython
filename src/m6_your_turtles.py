009695178c2f251f74221cb4ee7716419c7161ec
shenc4 <shenc4@rose-hulman.edu>
2018-11-28 00:38:44 -0500

    finished

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Caleb Shen.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,31 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+
+leonardo = rg.SimpleTurtle('turtle')
+leonardo.pen = rg.Pen('green', 2)
+leonardo.speed = 15
+
+size = 150
+
+for k in range(20):
+
+        leonardo.draw_circle(size)
+
+        leonardo.pen_up()
+        leonardo.forward(20)
+        leonardo.right(60)
+        leonardo.pen_down()
+        size = size - 4
+
+window.tracer(100)
+michaelangelo = rg.SimpleTurtle('square')
+michaelangelo.pen = rg.Pen('blue', 2)
+michaelangelo.forward(25)
+
+for k in range(450):
+    michaelangelo.right(65)
+    michaelangelo.forward(k)
+window.close_on_mouse_click()
